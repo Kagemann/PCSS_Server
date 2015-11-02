82bb064946f663b86742f6390f0b4f239a547233
Kagemann <krijen14@student.aau.dk>
2015-11-02 13:25:34 +0100

    DevelopmentCard and Interface is added.

---
+++ src/com/company/DevelopmentCardIF.java
@@ -0,0 +1,26 @@
+package com.company;
+
+/**
+ * Created by kristianhjensen on 02/11/2015.
+ */
+
+/**
+ * This is an interface for all development cards which will be used in our game
+ */
+public interface DevelopmentCardIF {
+    //Lowest known number
+    public final int Min = 0;
+    //Knight
+    public final int Blackhole = 0;
+    //Road
+    public final int Road = 2;
+    //Monopoly
+    public final int Mono = 3;
+    //Unknown to report to other players
+    public final int Unknown = 4;
+    //Highest known number
+    public final int Max = 5;
+
+
+
+}
