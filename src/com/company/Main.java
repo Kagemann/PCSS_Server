212cd7da40e081c4b1e6e0d8eb2b2ae7679baaf3
ThatGuyWithTheBeard <nicklas.kristoffersen@hotmail.com>
2015-11-03 16:38:10 +0100

    InitServer
    Har tilføjet kode i Main classen som kører kode fra InitServerNetwork
    classen.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,18 @@
 package com.company;
 
-/*public class Main {
+public class Main {
 
     public static void main(String[] args) {
-
-
-
-
+    	
+    	InitServerNetwork lobby = new InitServerNetwork();
+		
+		lobby.initServer(InitServerNetwork.getPort());
+		
+		do {
+			
+			lobby.handleClient();
+			
+		} while(true);
+    	
     }
-}*/
+}
